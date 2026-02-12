aab9fc7980abc7df6acc3abe6f81d2b07d645283
Oujaa <ayaoujaa194@gmail.com>
2026-02-12 10:21:44 +0100

    service arbo

--- src/service/Arborescence.java
+++ src/service/Arborescence.java
@@ -1,5 +1,6 @@
 package service;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -9,76 +10,65 @@ import model.Model;
 import model.Produit;
 
 public class Arborescence {
-	 private Model model;
+	 private static Model model;
 
 	 public Arborescence(Model model) {
 	     this.model = model;
-	 }
+	 }  
 
+	 
+	 
 	    // RECUP LES DONNEES
 
 	 public void chargerDonnees() throws SQLException {
 	     model.getAll();
 	 }
 
+	 
+	 
 	
-	 public void recup(int id_categ) throws SQLException,ChampInvalide {
-		
-			 
-			 if ( id_categ <=0) {
-				 throw new ChampInvalide("l'id doit etre superieur ou egal a 1");
-			 }
-			 
-		 
-	        model.recupId(id_categ);
-	    }
 	 
 	 
 	 
 	 
-	 
-	public static void arborescence(Categorie categorie, List<Categorie> listCateg, List<Produit>listProduit, String indent) //indentation 
-	{
-		
-		// affiche la categ
-		System.out.println(indent + "|__ C" + categorie.getId_categ()+" : "+ categorie.getNom() );
-		
-		
-		// affiche les produits lié a la categ
-		for(Produit p:listProduit) {
-			if(p.getId_categ() == categorie.getId_categ()) {
-				 System.out.println(indent + "   |__ P" + p.getId_produit() + " : "+ p.getNom());
-			}
+	 public static void afficheArborescence(int id_categ, String indent) throws ChampInvalide, SQLException {
+
+		    
+		    if (id_categ <= 0) {
+		        throw new ChampInvalide("l'id doit être supérieur ou égal à 1");
+		    }
+
+		    
+		    // recup la categ entrée
+		    ResultSet recupCateg = model.categorieId(id_categ);
+		    if (recupCateg.next() == false) {
+		    	
+		        throw new ChampInvalide("la catégorie n'existe pas");
+		    }
+
+		    System.out.println(indent + "   |__ C" + recupCateg.getInt("id_categ") + " : " + recupCateg.getString("nom"));
+
+		    
+		    // produit de la categ 
+		    ResultSet recupProd = model.ProduitParCateg(id_categ);
+		    while (recupProd.next()) {
+		    	
+		        System.out.println(indent + "       |__ P" + recupProd.getInt("id_produit") + " : " + recupProd.getString("nom"));
+		        
+		    }
+
+		    
+		    // recup tt les sous categ
+		    ResultSet recupSousCateg = model.sousCategorieId(id_categ);
+		    while (recupSousCateg.next()) {
+		    	
+		        int sousCategId = recupSousCateg.getInt("id_categ");
+		        
+		        // recursivité 
+		        afficheArborescence(sousCategId, indent + "    ");
+		    }
 		}
-		
-		
-		// parcours les categ pour trouver des sous categ de la categ mere
-		for(Categorie c2: listCateg) {
-			if(c2.getId_parent() == categorie.getId_categ()) { 
-				
-				 arborescence(c2, listCateg, listProduit, indent+"      ");
-			}
-		} 
-	}
+
 	
 	
-	
-	public void afficherDepuisId(int idCateg) throws ChampInvalide {
-        Categorie racine = null;
-
-        for (Categorie c : model.getListCateg()) {
-            if (c.getId_categ() == idCateg) {
-                racine = c;
-                break;
-            }
-        }
-
-        if (racine == null) {
-            throw new ChampInvalide("Catégorie inexistante !");
-        }
-
-        // Appel de ta méthode existante
-        arborescence(racine, model.getListCateg(), model.getListProduit(), "   ");
-    }
-
 }
